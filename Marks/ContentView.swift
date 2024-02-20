1ed6d9d7c0d4dd38bf5f58a5bf3c9cde5db9a938
lvbingkkk <443647090@qq.com>
2024-02-20 18:43:42 +0800

    Initial Commit

---
+++ Marks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marks
+//
+//  Created by Sargerace on 2024/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marks/MarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarksApp.swift
+//  Marks
+//
+//  Created by Sargerace on 2024/2/20.
+//
+
+import SwiftUI
+
+@main
+struct MarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
